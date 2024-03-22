2444b8c71c41605a45756bae9cf0fe9e27588e87
Al-ameen Olajide <81990934+Alinxus@users.noreply.github.com>
2024-03-22 20:47:23 +0000

    main

--- grazia/app/layout.tsx
+++ grazia/app/layout.tsx
@@ -5,6 +5,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -23,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <body className={cn(inter.className, "h-full")}>
-        <Providers>{children}<Analytics /></Providers>
+        <Providers>{children}<Analytics /><SpeedInsights /></Providers>
       </body>
     </html>
   );
