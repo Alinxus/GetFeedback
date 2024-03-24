ff76a275515614a5ff36dcaf69d84209847e2276
Al-ameen Olajide <81990934+Alinxus@users.noreply.github.com>
2024-03-24 04:45:07 +0000

    main

--- grazia/grazias/pages/api/auth/[...nextauth].js
+++ grazia/grazias/pages/api/auth/[...nextauth].js
@@ -2,13 +2,13 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth from "next-auth";
 import EmailProvider from "next-auth/providers/email";
 import GoogleProvider from "next-auth/providers/google";
-import { prisma } from "@/prisma";
+import prisma from "@/lib/prismaClient";
 
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     EmailProvider({
       server: {
@@ -32,5 +32,24 @@ export default NextAuth({
     brandColor: "#FFFFFF",
     logo: "/favicon.ico",
     buttonText: "#000000",
+  },
+  callbacks: {
+    async session({ session, token }) {
+      session.user.id = token.user.id;
+
+      return session;
+    },
+    async jwt({ token, account, user }) {
+      if (user) {
+        if (!token.user) {
+          token.user = {
+            id: "",
+          };
+        }
+
+        token.user.id = user.id;
+      }
+      return token;
+    },
   },
 });
